4e48e64795ceb77abc7582ac2323c460fcc8f2f0
Emily Chapman <emilychapman@Emilys-MacBook-Air.local>
2015-11-16 18:45:16 -0500

    Adding slug route

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -8,4 +8,9 @@ class PinsController < ApplicationController
     @pin = Pin.find(params[:id])
   end
   
+  def show_by_name
+  	@pin = Pin.where(slug: params[:slug])[0]
+  	render :show
+  end
+  
 end
